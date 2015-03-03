424cc5b1f86ae5f354bef5ac67a2a11d3e52c849
C.W. Betts <computers57@hotmail.com>
2015-03-02 23:12:32 -0700

    Port testLargeMul to Swift.

--- BigInteger-MacOSX Framework Tests/BISwiftTesting.swift
+++ BigInteger-MacOSX Framework Tests/BISwiftTesting.swift
@@ -39,4 +39,42 @@ class BISwiftTesting: XCTestCase {
 		XCTAssert(bii == -bij)
 		
 	}
+	
+	func testLargeMultiply() {
+		for (var i: Int32 = 10; i <= 10240; i *= 2) {
+			autoreleasepool() {
+				for j in Int32(0)..<Int32(200) {
+					let bi = BigInteger(randomNumberOfSize: i, exact: true)
+					let bj = BigInteger(randomNumberOfSize: i, exact: true)
+					let bk = BigInteger(randomNumberOfSize: i >> 1, exact: true)
+					
+					var br = bi * bj
+					var bs = bj * bi
+					XCTAssert(br == bs)
+					var bd = br / bi
+					XCTAssert(bd == bj)
+					
+					br = bi * bk
+					bs = bk * bi
+					XCTAssert(br == bs)
+					bd = br / bi
+					XCTAssert(bd == bk)
+				}
+			}
+		}
+		var bi = BigInteger(string: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", radix: 16)
+		var bj = BigInteger(string: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", radix: 16)
+		if let bi = bi, bj = bj {
+			let br = bi * bj
+			let bs = bj * bi
+			XCTAssert(br == bs)
+			if let radix = br.toRadix(10) {
+				XCTAssert(radix == "115792089237316195423570985008687907852589419931798687112530834793049593217025")
+			} else {
+				XCTAssertFalse(false, "Unable to convert to radix.")
+			}
+		} else {
+			XCTAssertFalse(false, "bi and/or bj returned nil, bi: \(bi), br: \(bj)")
+		}
+	}
 }
